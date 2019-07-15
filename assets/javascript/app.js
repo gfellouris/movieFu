33a289b2e1fd85b6810af4503488f33df088988c
Eric Thiel <ethiel172@gmail.com>
2019-07-15 14:40:53 -0400

    fix 3?

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
 
 
 
-  // AJAX CALLS
+    // AJAX CALLS
 
     // AJAX CALL FOR UTELLY API
     // Leave commented during development to reduce amount of calls made
@@ -36,34 +36,34 @@ $(document).ready(function () {
       console.log(data);
 
 
-// Creating a varible tht will be assigneto the #provider1 div
-      var iconDiv1 = $("#icon1")
-// Grabing utelly data and pulling from the it the icon property
-      var icon1 = data.results[0].locations[0].icon
-// Attributing to iconimg1 an img and attribute search the url from the utelly data response
+      // Creating a varible tht will be assigneto the #provider1 div
+      var iconDiv1 = $("#icon1");
+      // Grabing utelly data and pulling from the it the icon property
+      var icon1 = data.results[0].locations[0].icon;
+      // Attributing to iconimg1 an img and attribute search the url from the utelly data response
       var iconIMG1 = $("<img>").attr("src", icon1);
-// Appending the img to the iconDiv variable
-      iconDiv1.append(iconIMG1)
-      console.log(icon1)
+      // Appending the img to the iconDiv variable
+      iconDiv1.append(iconIMG1);
+      console.log(icon1);
 
-// Creating a varible tht will be assigneto the #provider1 div
-      var iconDiv2 = $("#icon2")
-// Grabing utelly data and pulling from the it the icon property
-      var icon2 = data.results[0].locations[1].icon
-// Attributing to iconimg1 an img and attribute search the url from the utelly data response
+      // Creating a varible tht will be assigneto the #provider1 div
+      var iconDiv2 = $("#icon2");
+      // Grabing utelly data and pulling from the it the icon property
+      var icon2 = data.results[0].locations[1].icon;
+      // Attributing to iconimg1 an img and attribute search the url from the utelly data response
       var iconIMG2 = $("<img>").attr("src", icon2);
-// Appending the img to the iconDiv variable
-      iconDiv2.append(iconIMG2)
-      console.log(icon2)     
+      // Appending the img to the iconDiv variable
+      iconDiv2.append(iconIMG2);
+      console.log(icon2);
 
-    
 
-// Preparing to append data from the utelly api call to provider1 div.
-      $("#provider1").html( data.results[0].locations[0].display_name);
-      console.log( "Steaming Service: " + data.results[0].locations[0].display_name)
-// Preparing to append data from the utelly api call to provider2 div.     
-     $("#provider2").html( data.results[0].locations[1].display_name);
-      console.log(data.results[0].locations[1].display_name)
+
+      // Preparing to append data from the utelly api call to provider1 div.
+      $("#provider1").html(data.results[0].locations[0].display_name);
+      console.log("Steaming Service: " + data.results[0].locations[0].display_name)
+      // Preparing to append data from the utelly api call to provider2 div.     
+      $("#provider2").html(data.results[0].locations[1].display_name);
+      console.log(data.results[0].locations[1].display_name);
     });
 
 
@@ -73,42 +73,42 @@ $(document).ready(function () {
       method: "GET"
     }).then(function (omdbResponse) {
       console.log(omdbResponse);
-    // Creates Variable omdbDIV to a new div that will append to poster div
-      var omdbDiv = $("#poster")
-    // Creates Varible poster to hold the poster img retrieved from omdb API
-      var poster = omdbResponse.Poster
+      // Creates Variable omdbDIV to a new div that will append to poster div
+      var omdbDiv = $("#poster");
+      // Creates Varible poster to hold the poster img retrieved from omdb API
+      var poster = omdbResponse.Poster;
       var image = $("<img>").attr("src", poster);
-    // Appends poster image to omdb Div
+      // Appends poster image to omdb Div
       omdbDiv.html(image);
 
-      
-        // For Grabbing Movie Details - OMDB API Reponse Needed
 
-// Grabs omddAPI response.Title and appends it to #title div
-      var title =  omdbResponse.Title
+      // For Grabbing Movie Details - OMDB API Reponse Needed
+
+      // Grabs omddAPI response.Title and appends it to #title div
+      var title = omdbResponse.Title;
       $("#title").html("Title: " + title);
-// Grabs omddAPI response.Released and appends it to #relased div
-      var released = omdbResponse.Released
+      // Grabs omddAPI response.Released and appends it to #relased div
+      var released = omdbResponse.Released;
       $("#released").html("Released: " + released);
-//  Grabs omddAPI response.Rated and appends it to #rated div
-      var rated = omdbResponse.Rated
+      //  Grabs omddAPI response.Rated and appends it to #rated div
+      var rated = omdbResponse.Rated;
       $("#rated").html("Rating: " + rated);
-//  Grabs omddAPI response.Runtime and appends it to #runtime div     
-      var runtime = omdbResponse.Runtime
+      //  Grabs omddAPI response.Runtime and appends it to #runtime div     
+      var runtime = omdbResponse.Runtime;
       $("#runtime").html("Run Time: " + runtime);
-// Grabs omdbAPI response.Genre and appends it to #genre div
-//      var genre = omdbResponse.Genre
-//      $("#genre") = append("Genre: " + genre);
+      // Grabs omdbAPI response.Genre and appends it to #genre div
+      //      var genre = omdbResponse.Genre
+      //      $("#genre") = append("Genre: " + genre);
 
       console.log("Title: " + title);
       console.log("Released: " + released);
       console.log("Rated: " + rated);
       console.log("Run Time: " + runtime);
-//      console.log("Genre: " + genre);
-//      $("movie-details").append(omdbDiv)
+      //      console.log("Genre: " + genre);
+      //      $("movie-details").append(omdbDiv)
     });
 
-    
+
 
     // Referencing a JSON example of an OMDB AJAX response
     // Just in case OMDB's API isn't working 
